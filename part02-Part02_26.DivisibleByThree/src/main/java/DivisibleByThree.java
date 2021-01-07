29923fa02817e8ccccd2176455ab15c4a783dbfc
Raul Peña <tatenokay@gmail.com>
2021-01-07 17:37:12 -0500

    Update DivisibleByThree.java

--- part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -5,6 +5,17 @@ public class DivisibleByThree {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a Numbers: ");
+        int beginning = Integer.valueOf(scanner.nextLine());
+        int end = Integer.valueOf(scanner.nextLine());
+        divisibleByThreeInRange(beginning , end);
+    }
+    public static void divisibleByThreeInRange(int beginning, int end){
+        for (int i = beginning; i <= end; i++){
+            if (i % 3 == 0){
+                System.out.println(""+i);
+            }
+        }
 
     }
 
