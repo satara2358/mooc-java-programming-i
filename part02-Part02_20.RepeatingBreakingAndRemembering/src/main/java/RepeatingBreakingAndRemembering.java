46fbff32c537787ac4bf36c6cb26af49c5df70f3
Raul Peña <tatenokay@gmail.com>
2021-01-06 22:21:02 -0500

    Update RepeatingBreakingAndRemembering.java

--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -4,15 +4,37 @@ import java.util.Scanner;
 public class RepeatingBreakingAndRemembering {
 
     public static void main(String[] args) {
-        
-        // This exercise is worth five exercise points, and it is 
-        // gradually extended part by part.
-        
-        // If you want, you can send this exercise to the server
-        // when it's just partially done. In that case the server will complain about 
-        // the parts you haven't done, but you'll get points for the finished parts.
-        
         Scanner scanner = new Scanner(System.in);
+        int cont = 0;
+        int i = 0;
+        double prom = 0;
+        int par = 0;
+        int imp = 0;
+        while (true){
+            System.out.println("Give numbers: ");
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num != -1){
+                if (num % 2 == 0){
+                    par ++;
+                }else{
+                    imp ++;                    
+                }
+                i++;
+                cont = cont + num;
+                continue;
+            }
+            
+            if (num == -1){
+                prom = (double)cont / i ;
+                System.out.println("Thx! Bye!");
+                break;
+            }
+        }
+        System.out.println("Sum: "+cont);
+        System.out.println("Numbers: "+i);
+        System.out.println("Average: "+ prom);
+        System.out.println("Even: "+par);
+        System.out.println("Odd: "+imp);
 
     }
 }
