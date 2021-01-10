2c6cea4a6510f17155429a0270ae979af4560103
Raul Peña <tatenokay@gmail.com>
2021-01-10 17:33:55 -0500

    Update AdvancedAstrology.java

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -1,20 +1,60 @@
 
 public class AdvancedAstrology {
 
-    public static void printStars(int number) {
-        // part 1 of the exercise
+    for (int i = 1; i <= number; i++){
+            System.out.print("*");
+        }
+        System.out.println("");
     }
-
     public static void printSpaces(int number) {
-        // part 1 of the exercise
+        for (int i = 0; i < number; i++){
+            System.out.print(" ");
+        }
     }
-
     public static void printTriangle(int size) {
-        // part 2 of the exercise
+        /*int x =0;
+        int ad = 1;
+        int y = size;
+        while(x <= size){
+            printSpaces(y);
+            y--;
+            if (y<=18 && y >=0){
+                printStars(x);   
+            }
+            x++;
+            //ad++;  
+        }*/ 
+        for (int i = 1; i <= size; i++) {
+            printSpaces(size - i);
+            printStars(i);
+        }
     }
-
     public static void christmasTree(int height) {
-        // part 3 of the exercise
+        /**int x =0;
+        int ad = 1;
+        int y = height;
+        while(x <= height){
+            printSpaces(y);
+            y--;
+            if (y<=18 && y >=0){
+                printStars(x+ad);   
+            }
+            x++;
+            ad++; 
+        }
+        for(int i= 0; i < 2; i++){
+            printSpaces(height/2+1);
+            printStars(3);
+        }**/
+        for (int i = 1; i <= height; i++) {
+            printSpaces(height - i);
+            printStars(i+(i-1));
+        }
+
+        for (int i = 0; i < 2; i++) {
+            printSpaces(height - 2);
+            printStars(3);
+        }
     }
 
     public static void main(String[] args) {
